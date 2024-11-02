5969ca979f52593561401de5bde7cf6e15e95cca
Kieran Laverty <44180036+kieranlaverty@users.noreply.github.com>
2024-11-02 16:34:35 -0400

    This works
    THIS IS NOW THE MAIN BRANCH

--- AI.py
+++ AI.py
@@ -1,13 +1,19 @@
-
+from dotenv import load_dotenv
 import os
-from llama_index.embeddings.huggingface import HuggingFaceEmbedding
-from llama_index.llms.ollama import Ollama
+
+from llama_index.llms.openai import OpenAI
 
 from llama_index.core.tools import QueryEngineTool, ToolMetadata
 from llama_index.core.agent import ReActAgent
 
 from ai.pdf import book_engine
 from ai.prompt import new_prompt, instruction_str, context
+
+# Load environment variables from the .env file
+load_dotenv()
+client = OpenAI(
+    api_key=os.getenv("OPENAI_API_KEY"),
+)
 
 tools = [
     
@@ -20,7 +26,9 @@ tools = [
     ),
 ]
 
-llm = Ollama(model="llama3", request_timeout=360.0)
+llm =  OpenAI(
+    openai_api_key=os.environ["OPENAI_API_KEY"],
+    model="gpt-3.5-turbo")
 agent = ReActAgent.from_tools(tools, llm=llm, verbose=True, context=context)
 
 while (prompt := input("Enter a prompt (q to quit): ")) != "q":
--- ai/pdf.py
+++ ai/pdf.py
@@ -1,22 +1,16 @@
 import os
-from llama_index.core import StorageContext
-from llama_index.core import VectorStoreIndex, load_index_from_storage
+from llama_index.core import StorageContext, VectorStoreIndex, load_index_from_storage
 from llama_index.readers.file import PDFReader
 
-from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, Settings
-from llama_index.embeddings.huggingface import HuggingFaceEmbedding
-from llama_index.llms.ollama import Ollama
 
-# bge-base embedding model
-Settings.embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-base-en-v1.5")
-# ollama
-Settings.llm = Ollama(model="llama3", request_timeout=360.0)
+
+
 
 def index(data, name):
     index = None
     if not os.path.exists(name):
         print("creating index", name)
-        index = VectorStoreIndex.from_documents(data, show_progress=True)
+        index = VectorStoreIndex.from_documents( data, show_progress=True)
         index.storage_context.persist(persist_dir=name)
 
     else:
